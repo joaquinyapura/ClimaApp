1878c95b2a0f2602b1345f51fffb234e62acaab8
Joaquin Macbook <joaquin@gjm.com>
2024-06-21 04:38:56 -0300

    actualización de css para card y mejoras en dimensiones de textos

--- src/Components/Weater.jsx
+++ src/Components/Weater.jsx
@@ -8,20 +8,20 @@ export const Weater = ({data}) => {
     <div>
 
     {data.weather? (
-        <div className="w-[700px] h-[400] rounded-xl bg-white relative p-4 mt-4 text-center flex  justify-center text-gray-600" >
+        <div className="w-[700px] h-[400] rounded-xl bg-gray-300 relative px-14 mt-4 text-center flex  justify-between text-gray-600" >
 
 
-        <div className="w-1/2 flex-col m-auto text-left ">
-        <h1 className="text-5xl font-bold " >{data.name}</h1> 
+        <div className="w-1/2 flex flex-col justify-center  text-left ">
+        <h1 className="text-5xl font-bold " >{data.name}</h1> <span>{data.sys.country}</span> 
         <h3 className="text-2xl text-orange-600">{data.main.temp} C°</h3>
         </div>
 
-        <div className="w-1/2 flex-col">
-        <img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`} alt="" srcset="" />
+        <div className="w-1/2 flex justify-center ">
+        <img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`} className="w-2/3" />
         </div>
 
         </div>
-    ):null}
+    ): null}
 
     </div>
 
